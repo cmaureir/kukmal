ad7e4acc085c7667f987f23f1866856cda24aef8
Cristian (ger) <cmaureirafredes@gmail.com>
2013-02-05 00:59:33 +0100

    KuKmal 404 error page
    Signed-off-by: Cristian (ger) <cmaureirafredes@gmail.com>

---
+++ kukmal404.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+        <h1 class="title-404">
+		<?php _e('404 &#8212; Fancy meeting you here!', 'responsive'); ?>
+	</h1>
+
+        <p>
+		Zur Zeit befinden sich hier noch keine Eintr&auml;ge.
+		Wenn KuKmal gr&ouml;&szlig;er wird, &auml;ndert sich das.
+		<br/>
+		Empfehlen Sie KuKmal Ihren Freunden und Bekannten auf <a href="http://twitter.com/KuKMalOrg">Twitter</a>
+		und <a href="https://plus.google.com/u/0/107596387333091691616/about">Google+</a> und besuchen
+		Sie unsere <a href="http://www.facebook.com/pages/KuK-mal/235794786482778">Facebook-Seite</a>. Vielen Dank
+
+	</p>
+
+        <h6>
+        
+		<?php get_search_form(); ?>
+
+<?php
+
+?>
